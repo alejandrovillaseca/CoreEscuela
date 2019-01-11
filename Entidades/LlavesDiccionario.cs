c566e0cc4443136c0eedb5cd47d21267793b3696
Alejandro Villaseca <alejandrovs88@gmail.com>
2019-01-11 16:47:29 -0300

    Commit inicial

---
+++ Entidades/LlavesDiccionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etapa5.Entidades
+{
+    public struct LlavesDiccionario
+    {
+        public const string CURSOS = "Cursos";
+        public const string ALUMNOS = "Alumnos";
+    }
+}
